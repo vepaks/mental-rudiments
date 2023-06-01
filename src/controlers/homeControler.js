44452c7891ed26b3ed3e0cbee67353d9c2b21686
aleksandar kamburov <kamburoff@gmail.com>
2023-06-01 14:18:14 +0300

    add about page

--- src/controlers/homeControler.js
+++ src/controlers/homeControler.js
@@ -4,6 +4,10 @@ router.get( '/', (req, res) => {
     res.render('index');
 })
 
+router.get('/about', (req, res) => {
+    res.render('about')
+})
+
 router.get ('*', (req, res) => {
     res.render('404')
 })
