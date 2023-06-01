273bd8d95c54d1baa6f7c861a2ad1bf5d478e0fa
vepaks <kamburoff@gmail.com>
2023-06-01 13:32:46 +0300

    add express, handlebars config and home router

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
+
+const expressConfig = require('./config/expressConfig')
+const handlebarsConfig = require('./config/handlebarsConfig')
+
+const homeControler = require('./controlers/homeControler')
 
 const app = express();
 const PORT = 5000;
 
-// Static files
-app.use(express.static(path.resolve(__dirname, 'public')));
-
-// Handlebars configuration
-app.engine('hbs', handlebars.engine({
-    extname: '.hbs',
-}));
-
-app.set('view engine', 'hbs');
-app.set('views', 'src/views');
+expressConfig(app)
+handlebarsConfig(app)
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('index');
-})
+app.use(homeControler)
+
 app.get("*", (req, res) => {
     res.render('404');
 })
