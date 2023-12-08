feddc7d268231d0faf9e0cce138ee1ea14c2fec9
Aleksandar Kamburov <kamburoff@gmail.com>
2023-12-08 09:24:15 +0200

    clear server

--- src/config/dbConfig.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const URI = 'mongodb://127.0.0.1:27017/mental-rudiments'
-
-async function dbConnect (){
-    await mongoose.connect(URI)
-}
-
-module.exports = dbConnect;
--- src/config/expressConfig.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const path = require('path')
-
-function expressConfig (app) {
-    app.use(express.static(path.resolve(__dirname, '../public')));
-    app.use(express.urlencoded({ extended: false }));
-}
-module.exports = expressConfig
